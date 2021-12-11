024b6f749759d4675d7b61be6597f87a04f0c1b8
Danil Saturday <danil200subbotin@gmail.com>
2021-12-11 12:43:05 +0300

    simple parser

--- lab3/main.py
+++ lab3/main.py
@@ -1,17 +1,14 @@
-# This is a sample Python script.
+from lexer import Lexer
+from parser import Parser
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+text_input = """
+print(4 + 4 - 2);
+"""
 
+lexer = Lexer().get_lexer()
+tokens = lexer.lex(text_input)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--
+pg = Parser()
+pg.parse()
+parser = pg.get_parser()
+parser.parse(tokens).eval()
